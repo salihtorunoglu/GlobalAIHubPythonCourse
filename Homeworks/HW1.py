1794870cb38c3e30bf2f86ffa531c8966d5ec329
Salih Emre <63926446+salihtorunoglu@users.noreply.github.com>
2021-03-10 21:21:28 +0300

    Create HW1.py

---
+++ Homeworks/HW1.py
@@ -0,0 +1,8 @@
+MyList1 = [1,3,5,7]
+MyList2 = [2,4,6,8]
+MyList1.extend(MyList2) #I extended MyLis1 with MyList2
+print(MyList1) #I printed the extended list
+Multiplied_List=[element*2 for element in MyList1] #I mutiplied every variable in extended list with two
+print(Multiplied_List)
+for i in Multiplied_List: #I used a for loop to print the data type of the all values in Multiplied_List
+    print(i,"=",type(i))
