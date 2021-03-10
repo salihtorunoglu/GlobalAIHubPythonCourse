fd5049a2b1ddbe09e33e3d6ec4c293a16c14a2bb
Salih Emre <63926446+salihtorunoglu@users.noreply.github.com>
2021-03-10 20:33:08 +0300

    Delete Homeworks/HW1.py directory

--- Homeworks/HW1.py/main.py
+++
@@ -1,8 +0,0 @@
-myList=[1,3,5,7]
-myList1=[2,4,6,8]
-myList.extend(myList1) #First I extended myList with myList1
-print(myList)
-multiplied_list = [element * 2 for element in myList] #Then I multiplied the extended list with two
-print(multiplied_list)
-for i in multiplied_list:
-    print(i,"=",type(i))   #At last I printed the type of the list element
